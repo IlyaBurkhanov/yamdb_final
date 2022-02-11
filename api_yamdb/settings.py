b5eb3e8d8929c25b907a1afda088539cd777a122
iabur <ia@ia.ru>
2022-02-11 21:12:09 +0300

    test17

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -133,7 +133,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
